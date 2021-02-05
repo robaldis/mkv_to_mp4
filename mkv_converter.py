e8534e246c294a14d8398b3ca31ae84643a2b9b2
robaldis <robertaldis@gmail.com>
2021-02-05 19:49:18 +0000

    reoving mkv once converted

--- mkv_converter.py
+++ mkv_converter.py
@@ -6,6 +6,7 @@ def mkv_to_mp4(mkv_file):
     out_name = name + ".mp4"
     ffmpeg.input(mkv_file).output(out_name).compile(overwrite_output=False)
     print(f"Finished converting {mkv_file}")
+    os.remove(mkv_file)
 
 def search_sub_dir(directory):
     for root, dirs, files in os.walk(directory, topdown=False):
