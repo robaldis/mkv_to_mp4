dc6334319473ba93017ed4accc01472349227ebc
robaldis <robertaldis@gmail.com>
2021-02-05 10:35:55 +0000

    adding in directory search

--- mkv_converter.py
+++ mkv_converter.py
@@ -7,4 +7,11 @@ def mkv_to_mp4(mkv_file):
     ffmpeg.input(mkv_file).output(out_name).run()
     print(f"Finished converting {mkv_file}")
 
+def search_sub_dir(directory);
+    for root, dirs, files in os.walk(directory, topdown=False):
+        for name in files:
+            if name[:4:-1] == ".mkv":
+                print(f"Starting conversion on {name}")
+                mkv_to_mp4(name)
+
 
