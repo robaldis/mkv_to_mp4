d997003d4f24e65d95eea8d32d2b7a343e22a1e2
robaldis <robertaldis@gmail.com>
2021-02-05 11:08:53 +0000

    fixing directory searching and file detecting

--- mkv_converter.py
+++ mkv_converter.py
@@ -2,16 +2,23 @@ import os
 import ffmpeg
 
 def mkv_to_mp4(mkv_file):
-    name, ext = os.path.splittext(mkv_file)
+    print(mkv_file)
+    name, ext = os.path.splitext(mkv_file)
+    print(name, ext)
     out_name = name + ".mp4"
     ffmpeg.input(mkv_file).output(out_name).run()
     print(f"Finished converting {mkv_file}")
 
-def search_sub_dir(directory);
+def search_sub_dir(directory):
     for root, dirs, files in os.walk(directory, topdown=False):
+        print(root)
         for name in files:
-            if name[:4:-1] == ".mkv":
+            print(name[-4:])
+            if name[-4:] == ".mkv":
                 print(f"Starting conversion on {name}")
-                mkv_to_mp4(name)
+                mkv_to_mp4(os.path.join(root, name))
 
 
+if __name__ == "__main__":
+    print("starting")
+    search_sub_dir(os.path.join(os.getcwd(), "movies"))
